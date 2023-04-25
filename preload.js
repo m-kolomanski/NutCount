9c211c68abcfd9d6318a052818246d569a1b399a
m-kolomanski <m.kolomanski@yahoo.com>
2023-04-25 18:11:47 +0200

    Simple update checking

--- preload.js
+++ preload.js
@@ -1,9 +1,12 @@
-const { contextBridge, ipcRenderer} = require('electron')
+const { contextBridge, ipcRenderer} = require('electron');
 const path = require('path')
 
 window.dbmgr = require(path.join(__dirname, "/js/dbmgr.js"));
 window.fs = require('fs');
+window.path = require('path');
 window.utils = require(path.join(__dirname, "/js/utils.js"))
+
+window.ipcRenderer = ipcRenderer;
 
 fs.access(path.join(__dirname, './db/nuts.db'), fs.F_OK, (err) => {
 	const dbmgr = require(path.join(__dirname, '/js/dbmgr.js'));
