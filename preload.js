a527c4ed61e815d3b96d84c8a244f030876587aa
m-kolomanski <m.kolomanski@yahoo.com>
2023-11-12 10:27:58 +0100

    Repo cleanup; new database manager with database creation

--- preload.js
+++ preload.js
@@ -1,26 +1,11 @@
 const { contextBridge, ipcRenderer} = require('electron');
 const path = require('path')
+const Dbmgr = require(path.join(__dirname, "/components/Dbmgr"));
 
-window.dbmgr = require(path.join(__dirname, "/js/dbmgr.js"));
+
 window.fs = require('fs');
 window.path = require('path');
-window.utils = require(path.join(__dirname, "/js/utils.js"))
+window.dbmgr = new Dbmgr();
+//window.utils = require(path.join(__dirname, "/js/utils.js"))
 
 window.ipcRenderer = ipcRenderer;
-
-fs.access(path.join(__dirname, './db/nuts.db'), fs.F_OK, (err) => {
-	const dbmgr = require(path.join(__dirname, '/js/dbmgr.js'));
-		
-	if (err) {
-		let db = dbmgr.loadDatabase();
-		dbmgr.createNewDatabase();
-		window.first_time = true;
-	} else {
-		window.first_time = false; /// add true for temp for dev
- 	}
-});
-
-window.db = dbmgr.loadDatabase();
-try {
-	window.nuts = require(path.join(__dirname, "./db/nuts.json"));
-} catch (error) { };
