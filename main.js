b137596be61f352ad48ce341fc25326ba14198f1
m-kolomanski <m.kolomanski@yahoo.com>
2024-02-02 13:45:42 +0100

    Added logging to file

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain} = require('electron')
+const path = require('path');
+const fs = require('fs');
+
+const getTimestamp = function() {
+  const date = new Date();
+  return `${date.getFullYear()}-${String(date.getMonth()).padStart(2,"0")}-${String(date.getDate()).padStart(2,"0")} ${String(date.getHours()).padStart(2,"0")}:${String(date.getMinutes()).padStart(2,"0")}:${String(date.getSeconds()).padStart(2,"0")}`;
+}
 
 const createWindow = () => {
   const win = new BrowserWindow({
-   // width: 800,
-   // height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
@@ -12,21 +16,27 @@ const createWindow = () => {
     },
     icon: 'ico.png'
   })
-  
   win.maximize();
   win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
   createWindow()
-})
+});
+
+ipcMain.handle('log', async (event, msg, level) => {
+  fs.writeFile("./main.log", `${getTimestamp()} [${level}]: ${msg}\n`, {flag: "a"}, err => {
+    if (err)  console.error(err);
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
 	  app.quit();
   }
-})
+});
 
 ipcMain.handle('quitApp', async () => {
   app.quit();
-})
+});
+
--- preload.js
+++ preload.js
@@ -4,12 +4,23 @@ const Dbmgr = require(path.join(__dirname, "/components/Dbmgr"));
 const Navbar = require(path.join(__dirname, "/components/Navbar"));
 window.Datatable = require(path.join(__dirname, "/components/Datatable"));
 
-
 window.fs = require('fs');
 window.path = require('path');
 window.dbmgr = new Dbmgr();
 window.customElements.define('top-navbar', Navbar);
 window.customElements.define('data-table', Datatable);
 window.locale = require(path.join(__dirname, "/locale/", `${window.dbmgr.getConfig('lang')}.json`));
-
-window.ipcRenderer = ipcRenderer;
+window.Log = {
+    debug: (msg) => {
+        ipcRenderer.invoke('log', msg, "DEBG");
+    },
+    info: (msg) => {
+        ipcRenderer.invoke('log', msg, "INFO");
+    },
+    warn: (msg) => {
+        ipcRenderer.invoke('log', msg, "WARN");
+    },
+    error: (msg) => {
+        ipcRenderer.invoke('log', msg, "ERRR");
+    }
+}
