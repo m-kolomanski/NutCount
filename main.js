e00107291ceaba1b3194abb8fd6f6cf2201a73a9
m-kolomanski <m.kolomanski@yahoo.com>
2025-07-27 10:57:07 +0200

    fix: build process

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const isDev = process.env.NODE_ENV === 'development';
 
 let mainWindow;
 
 const createWindow = () => {
-  // Create the browser window
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: path.join(__dirname, 'src/assets/logo.png'),
+    icon: path.join(__dirname, 'src/assets/icon.ico'),
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true
     }
   });
 
-  mainWindow.loadURL('http://localhost:3000');
-  mainWindow.webContents.openDevTools();
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, 'dist-react/index.html'));
+  }
 };
 
 app.whenReady().then(() => {
   createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
 });
 
 // Quit when all windows are closed
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { ThemeProvider } from './contexts/ThemeContext'
 import AppNavbar from './components/Navbar'
 import TodayPage from './pages/TodayPage'
